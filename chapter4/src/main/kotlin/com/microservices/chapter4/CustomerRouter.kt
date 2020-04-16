a7d61921334e5ea6feca3409e106831fe393fc7a
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-04-16 23:22:05 +0300

    feat: complete chapter 4

---
+++ chapter4/src/main/kotlin/com/microservices/chapter4/CustomerRouter.kt
@@ -0,0 +1,22 @@
+package com.microservices.chapter4
+
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.router
+
+@Component
+class CustomerRouter(private var customerHandler: CustomerHandler) {
+    @Bean
+    fun customerRoutes(): RouterFunction<*> = router {
+        "/functional".nest {
+            "/customer".nest {
+                GET("/{id}", customerHandler::get)
+                POST("/", customerHandler::create)
+            }
+            "/customers".nest {
+                GET("/", customerHandler::search)
+            }
+        }
+    }
+}
